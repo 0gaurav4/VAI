6b306e1c85335fb3d476e044571dd8201fcbbbd4
mysgrown <gauravtsm@gmail.com>
2024-04-29 12:33:04 +0530

    footer

--- Tasks/videocompress.py
+++ Tasks/videocompress.py
@@ -4,8 +4,8 @@ def create_video_compression():
     from moviepy.editor import VideoFileClip
 
     video = VideoFileClip("car.mp4")
-    video = video.subclip(3, 4)
-    video = video.without_audio()
+    # video = video.subclip(3, 4)
+    # video = video.without_audio()
     video = video.resize(0.5)
     video = video.set_fps(30)
     video.write_videofile("compressed.mp4")
